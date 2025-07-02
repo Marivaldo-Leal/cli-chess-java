f32069b92d0742dcdb098a3b0e782e93f2b675f2
Marivaldo-Leal <marivaldo.leal@outlook.com>
2025-07-02 10:35:08 -0300

    feat(boardgame): create position

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("CLI Chess");
+
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
